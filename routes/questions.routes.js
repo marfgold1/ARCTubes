d3c714528f11adf59a22416b6945b5512533f012
luthfirestu <74096272+luthfirestu@users.noreply.github.com>
2021-04-03 18:20:32 +0700

    Update questions.routes.js

--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 const multer = Multer();
 
 // kalo mau pass error, pake ini dulu baru dijadiin response
-function getErrorData(err){
+function getErrorData(err) {
     let error = {};
     if (err.name === "MongoError" && err.code === 11000)
         error["slug"] = "Slug is not unique, please change it to another value";
@@ -20,80 +20,83 @@ function getErrorData(err){
 
 // CREATE (RANJABI)
 // Create client interface
-router.get("/manage/new", function(req, res, next){
-    // res.render("create.ejs", { data: {} });
+router.get("/manage/create", function(req, res, next) {
+    res.render("create.ejs", { data: {} });
 });
 // Create server API
-router.post("/manage/create", multer.none(), function(req, res, next){
+router.post("/manage/create", multer.none(), function(req, res, next) {
     // buat response, pastiin utk sukses, return status 204 dan json kosong. kalo error, return status 400 dan json key "errors" yang isinya getErrorData(err)
     // sebagai referensi:
-    /* Post.create(req.body)
-    .then(data => {
-        res.status(204).json();
-    })
-    .catch(err => {
-        res.status(400).json({ errors:getErrorData(err) }); 
-    });*/
+    const slug = req.params.slug;
+    Question.create(req.body)
+        .then(data => {
+            res.status(204).json();
+        })
+        .catch(err => {
+            res.status(400).json({ errors: getErrorData(err) });
+        });
 });
 
 // READ (LIZA)
 // List question
-router.get("/manage", function(req, res, next){
-    /* UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
-    Post.find()
-    .then(posts => {
-        res.render('index.ejs', {posts: posts});
-    })
-    .catch(err => {
-        console.error(err);
-    });
-    */
+router.get("/manage", function(req, res, next) {
+    // UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
+    Question.find()
+        .then(posts => {
+            res.render('manage.ejs', { posts: posts });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
+
 
 // UPDATE (FIFI)
 // Update client interface (tampilin form edit ke client)
-router.get("/manage/edit/:slug", function(req, res, next){
-    /* UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
+router.get("/manage/edit/:slug", function(req, res, next) {
+
     const slug = req.params.slug;
-    Post.findOne({ slug })
-    .then(post => {
-        if (post == null) {
-            return res.sendStatus(404);
-        }
-        res.render("edit.ejs", { post: post });
-    })
-    .catch(err => {
-        res.sendStatus(404);
-    });
-    */
+    Question.findOne({ slug })
+        .then(post => {
+            if (post == null) {
+                return res.sendStatus(404);
+            }
+            res.render("edit.ejs", { post: post });
+        })
+        .catch(err => {
+            res.sendStatus(404);
+        });
+
 });
 // Update API server (apply edit ke database via server)
-router.put("/manage/edit/:slug", multer.none(), function(req, res, next){
+router.put("/manage/edit/:slug", multer.none(), function(req, res, next) {
     // buat response, pastiin utk sukses, return status 204 dan json kosong. kalo error, return status 400 dan json key "errors" yang isinya getErrorData(err)
     // sebagai referensi:
-    /*
+
     const slug = req.params.slug;
-    Post.updateOne({ slug },
-        {
+    Question.updateOne({ slug }, {
             $set: {
-                title: req.body.title,
-                author: req.body.author,
-                content: req.body.content
+                soal: req.body.soal,
+                jawaban_benar: req.body.jawaban_benar,
+                jawaban_salah_1: req.body.jawaban_salah_1,
+                jawaban_salah_2: req.body.jawaban_salah_2,
+                jawban_salah_3: req.body.jawban_salah_3
+
             }
-        }, {runValidators: true})
-    .then(data => {
-        res.status(204).json();
-    })
-    .catch(err => {
-        res.status(400).json({ errors:getErrorData(err) });
-    });
-    */
+        }, { runValidators: true })
+        .then(data => {
+            res.status(204).json();
+        })
+        .catch(err => {
+            res.status(400).json({ errors: getErrorData(err) });
+        });
+
 });
 
 
 // DELETE (FAZA)
 // Delete API server
-router.delete("/manage/delete/:slug", function(req, res, next){
+router.delete("/manage/delete/:slug", function(req, res, next) {
     /* UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
     const slug = req.params.slug;
     Post.deleteOne({ slug })
