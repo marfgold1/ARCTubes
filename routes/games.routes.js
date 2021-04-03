ac360665fe7dda2709a76a29ad45bde862c8db82
marfgold1 <34912365+marfgold1@users.noreply.github.com>
2021-04-03 20:49:37 +0700

    Small bugfix, working delete

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,33 +1,37 @@
 import { Router } from 'express';
 import Question from '../models/question.model.js';
-import Multer from 'multer';
 
 const router = Router();
-const multer = Multer();
 
 const mode = [
     {
-        label: "Santuy (5 Pertanyaan, 3 Menit)",
+        label: "Santuy (5 Pertanyaan, 1 Menit)",
         question: 5,
         time: 1,
         value: 0
     },
     {
-        label: "Manusia (10 Pertanyaan, 7 Menit)",
+        label: "Manusia (10 Pertanyaan, 3 Menit)",
         question: 10,
-        time: 7,
+        time: 3,
         value: 1
     },
     {
-        label: "Big bren (15 Pertanyaan, 10 menit)",
+        label: "Big bren (15 Pertanyaan, 5 menit)",
         question: 15,
-        time: 10,
+        time: 5,
         value: 2
     },
     {
         label: "Keos (20 Pertanyaan, 5 menit)",
         question: 20,
         time: 5,
+        value: 3
+    },
+    {
+        label: "SPEEDORUNN (20 Pertanyaan, 2 menit)",
+        question: 20,
+        time: 2,
         value: 3
     }
 ]
@@ -42,7 +46,6 @@ router.get("/play", async(req, res, next) => {
         if (playmode == undefined)
             res.redirect("/");
         const data = await Question.aggregate().sample(5).exec();
-        console.log(playmode);
         res.render("play.ejs", {data: data, mode: playmode});
     } else
         res.redirect("/");
--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -25,8 +25,6 @@ router.get("/manage/create", function(req, res, next) {
 });
 // Create server API
 router.post("/manage/create", multer.none(), function(req, res, next) {
-    // buat response, pastiin utk sukses, return status 204 dan json kosong. kalo error, return status 400 dan json key "errors" yang isinya getErrorData(err)
-    // sebagai referensi:
     const slug = req.params.slug;
     Question.create(req.body)
         .then(data => {
@@ -40,10 +38,9 @@ router.post("/manage/create", multer.none(), function(req, res, next) {
 // READ (LIZA)
 // List question
 router.get("/manage", function(req, res, next) {
-    // UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
     Question.find()
-        .then(posts => {
-            res.render('manage.ejs', { posts: posts });
+        .then(datas => {
+            res.render('manage.ejs', { datas: datas });
         })
         .catch(err => {
             console.error(err);
@@ -54,7 +51,6 @@ router.get("/manage", function(req, res, next) {
 // UPDATE (FIFI)
 // Update client interface (tampilin form edit ke client)
 router.get("/manage/edit/:slug", function(req, res, next) {
-
     const slug = req.params.slug;
     Question.findOne({ slug })
         .then(post => {
@@ -70,9 +66,6 @@ router.get("/manage/edit/:slug", function(req, res, next) {
 });
 // Update API server (apply edit ke database via server)
 router.put("/manage/edit/:slug", multer.none(), function(req, res, next) {
-    // buat response, pastiin utk sukses, return status 204 dan json kosong. kalo error, return status 400 dan json key "errors" yang isinya getErrorData(err)
-    // sebagai referensi:
-
     const slug = req.params.slug;
     Question.updateOne({ slug }, {
             $set: {
@@ -80,7 +73,7 @@ router.put("/manage/edit/:slug", multer.none(), function(req, res, next) {
                 jawaban_benar: req.body.jawaban_benar,
                 jawaban_salah_1: req.body.jawaban_salah_1,
                 jawaban_salah_2: req.body.jawaban_salah_2,
-                jawban_salah_3: req.body.jawban_salah_3
+                jawaban_salah_3: req.body.jawaban_salah_3
 
             }
         }, { runValidators: true })
@@ -97,16 +90,8 @@ router.put("/manage/edit/:slug", multer.none(), function(req, res, next) {
 // DELETE (FAZA)
 // Delete API server
 router.delete("/manage/delete/:slug", function(req, res, next) {
-    /* UNTUK REFERENSI, JANGAN LANGSUNG COPAS!! ngertiin dulu kodenya hehe terus sesuaikan oke?
     const slug = req.params.slug;
-    Post.deleteOne({ slug })
-    .then(data => {
-        res.status(204).json();
-    })
-    .catch(error => {res.sendStatus(500); console.error(error);})
-    */
-    const slug = req.params.slug;
-    Manage.deleteOne({ slug })
+    Question.deleteOne({ slug })
     .then(data => {
         res.status(204).json();
     })
